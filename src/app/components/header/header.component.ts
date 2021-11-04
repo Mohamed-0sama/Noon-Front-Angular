1d655b55a1ddb1b69bfdf844cf0a6a88f72cda30
Abdelmomen-Nasreldin <abdelmomennasreldin@gmail.com>
2021-11-04 16:57:59 +0200

    header_done_ui

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
+export class HeaderComponent implements OnInit, AfterViewInit {
+@ViewChild('second__navbar') second__navbar! : ElementRef
+@ViewChild('li') li! : ElementRef
+@ViewChild('show__categories') show__categories! : ElementRef
+  constructor() {}
+  toggleNav(){
+    if (this.second__navbar.nativeElement.style.marginLeft == "-160px") {
+      this.second__navbar.nativeElement.style.marginLeft = "0"
+    }else {
+      this.second__navbar.nativeElement.style.marginLeft = "-160px"
+    }
+  }
   ngOnInit(): void {
   }
-
+ngAfterViewInit(){
+this.li.nativeElement.onmouseover = () =>{
+this.show__categories.nativeElement.style.display = "flex"
+}
+this.show__categories.nativeElement.onmouseover = () =>{
+this.show__categories.nativeElement.style.display = "flex"
+}
+this.li.nativeElement.onmouseleave = () =>{
+this.show__categories.nativeElement.style.display = "none"
+}
+this.show__categories.nativeElement.onmouseleave = () =>{
+this.show__categories.nativeElement.style.display = "none"
+}
+}
 }
