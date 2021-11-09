7aff545ebb69f10039e7817f732dc0bca0ab932b
Azaytoun <ahmedzytoon95@gmail.com>
2021-11-09 17:58:55 +0200

    first commit to zaytoun branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './components/category/category.component';
+import { MobilesComponent } from './components/mobiles/mobiles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    MobilesComponent
   ],
   imports: [
     BrowserModule,
